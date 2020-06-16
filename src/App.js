bf3fddedee79e8a57301bb63f99cefdcf914d1ac
JaoVizu <joaovizu2015@gmail.com>
2020-06-15 23:17:31 -0300

    Criando componente de pesquisa

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React, { useState,useEffect } from 'react';
 // services
 import VideoApi from './services/api'
 
+// components
 import VideoCard from './components/VideoCard'
+import Search from './components/Search'
 
 function App() {
 
@@ -15,6 +17,7 @@ function App() {
 
   return (
     <div className="container mx-auto">
+      <Search />
       <div className="grid grid-cols-4 gap-3">
         {videos.map(video => (
           <VideoCard video={video}/>
